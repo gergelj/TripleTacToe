ab24161ce8065a6daac4b4c25d16096b5c6c1ed8
Gergo <49314980+gregVader@users.noreply.github.com>
2021-07-25 18:22:28 +0200

    feat: client: logo

--- triple-tac-toe/src/App.vue
+++ triple-tac-toe/src/App.vue
@@ -17,3 +17,15 @@ export default {
   })
 }
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: "Pixelmania";
+  src: local("Pixelmania"), local("Pixelmania"),
+    /*url($font_path+"Roboto-black.woff2") format("woff2"),
+    url($font_path+"Roboto-black.woff") format("woff"),*/
+    url("assets/fonts/Pixelmania.ttf") format("truetype");
+  font-style: normal;
+  font-weight: 900;
+}
+</style>
---
+++ triple-tac-toe/src/components/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 id="tripletactoe" class="text-center">TRIPLE=TAC=TOE</h1>
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style>
+  #tripletactoe {
+    font-family: "Pixelmania", Helvetica, Arial;
+    background-image: linear-gradient(90deg, #ff5252, #1976d2);
+    display: inline-block;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-text-fill-color: transparent;
+  }
+  @media (max-width: 550px) {
+    #tripletactoe { font-size: 1em; }
+  }
+
+  @media (min-width: 551px) {
+    #tripletactoe { font-size: 1.5rem; }
+  }
+</style>
